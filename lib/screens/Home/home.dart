9381af17eae70c36d708b523b49dec0979b1d297
iamdanielkissiedu <iamdanielkissiedu@gmail.com>
2024-03-22 14:55:21 +0000

    Added popular drones

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -1,23 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/core/styles.dart';
+import 'package:myapp/model/drone_model.dart';
+import 'package:myapp/screens/Home/Widgets/arrow_widget.dart';
+import 'package:myapp/screens/Home/Widgets/popular_drone_widget.dart';
 import 'package:myapp/screens/Home/Widgets/search_filed_widget.dart';
+import 'package:get/get.dart';
+import 'package:flutter/rendering.dart';
+import 'package:myapp/screens/Home/base_scroll_screen.dart';
 import '../../path.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class HomeController extends GetxController {
+  final scrollController = ScrollController();
+
+  /// width of the drone at ListView.builder position
+  double pieceWidth = 100.0;
+  var currentIndex = 0.obs;
+
+  void setCurrentIndex(int index) {
+    currentIndex.value = index;
+  }
+
+  void setPieceWidth(double containerWidth) {
+    pieceWidth = containerWidth;
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: _buildBody(context));
+  void onInit() {
+    scrollController.addListener(() {
+      final metrics = scrollController.offset;
+      final result = metrics ~/ pieceWidth;
+
+      final scrollingToRight = scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse;
+
+      if (scrollingToRight && result > currentIndex.value ||
+          !scrollingToRight && result < currentIndex.value) {
+        setCurrentIndex(result);
+      }
+    });
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
   }
 }
 
-// ignore: camel_case_types
-class _buildBody extends StatelessWidget {
-  const _buildBody(BuildContext context);
+class Home extends StatelessWidget {
+  Home({Key? key}) : super(key: key);
+  final HomeController controller = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
+    return BaseScrollScreen(
+      showBottomNavigationBar: true,
+      child: IntrinsicHeight(
+        child: _buildBody(context),
+      ),
+    );
+  }
+
+  Widget _buildHorizontalPopularDrones(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        // maxWidth: MediaQuery.of(context).size.width,
+        // minWidth: MediaQuery.of(context).size.width,
+        maxHeight: MediaQuery.of(context).size.width * 0.75,
+        minHeight: MediaQuery.of(context).size.width * 0.75,
+
+        // maxHeight: MediaQuery.of(context).size.width
+        // minHeight: MediaQuery.of(context).size.width ,
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          // int index = -1;
+          return Stack(
+            fit: StackFit.expand,
+            children: [
+              ListView.builder(
+                controller: controller.scrollController,
+                itemCount: droneModelsMock.length,
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemBuilder: (_, index) =>
+                    _buildDrone(context, droneModelsMock[index], index),
+              ),
+              const Positioned(
+                bottom: 0,
+                right: kDefaultMargin,
+                child: ArrowsWidget(turnOnRight: true),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildDrone(BuildContext context, DroneModel drone, int index) {
+    return Obx(() {
+      final isFirstDroneAtList = controller.currentIndex.value == index;
+      final popularDroneWidth = MediaQuery.of(context).size.width / 1.8;
+
+      controller.setPieceWidth(popularDroneWidth);
+
+      return PopularDroneWidget(
+        drone: drone,
+        isFirstDroneAtList: isFirstDroneAtList,
+        width: popularDroneWidth,
+      );
+    });
+  }
+
+  Widget _buildBody(BuildContext context) {
     return SafeArea(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,7 +141,9 @@ class _buildBody extends StatelessWidget {
                   style: kSubTitleTextStyle,
                   children: [TextSpan(text: 'Drone', style: kTitleTextStyle)]),
             ),
-          )
+          ),
+          const SizedBox(height: kDefaultMargin),
+          _buildHorizontalPopularDrones(context)
         ],
       ),
     );
